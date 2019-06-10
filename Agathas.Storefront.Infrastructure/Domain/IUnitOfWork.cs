40469e345792dbbdc70d22fe3f857ccf23ec5f79
Will Sams <willsams@hotmail.com>
2019-06-10 09:57:56 -0400

    Did some code & logic cleanup: - Moved ProperyNameHelper to Agathas.Storefront.Infrastructure.Helpers - Moved Unit-of-work logic back to Agathas.Storefront.Infrastucture.Repository.NHibernate - Began working on the configuration piece.  Will need to make more .NET Core friendly.  On-going.

---
+++ Agathas.Storefront.Infrastructure/Domain/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Agathas.Storefront.Infrastructure.Domain {
+  public interface IUnitOfWork{
+    void RegisterAmended(IAggregateRoot entity,
+                          IUnitOfWorkRepository unitofWorkRepository);
+    void RegisterNew(IAggregateRoot entity,
+                      IUnitOfWorkRepository unitofWorkRepository);
+    void RegisterRemoved(IAggregateRoot entity,
+                          IUnitOfWorkRepository unitofWorkRepository);
+    void Commit();
+  }
+}
+
---
+++ Agathas.Storefront.Infrastructure/Domain/IUnitOfWorkRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Agathas.Storefront.Infrastructure.Domain {
+  public interface IUnitOfWorkRepository {
+    void PersistCreationOf(IAggregateRoot entity);
+    void PersistUpdateOf(IAggregateRoot entity);
+    void PersistDeletionOf(IAggregateRoot entity);
+  }
+}
